eef32f38f2b4a999dfb4fe6c5879c5e8fbff340a
AhirHasmukh <ahir.hasmukh@gmail.com>
2014-09-29 21:36:13 +0530

    added bus tab and bus form design

--- cab/WebContent/js/javaScript.js
+++ cab/WebContent/js/javaScript.js
@@ -13,12 +13,16 @@ $('document').ready(function(){
 	/*  Taxi Design  */
 	$('.point-to-point').click(function(){
 		$('[class^="taxi-"]').hide();
+		$('[class^="bus-"]').hide();
 		$('.taxi-pop').fadeIn(300);
+		$('.bus-pop').fadeIn(300);
 	});
 	$('.airport').click(function(){
 		$('[class^="taxi-"]').hide();
 		$('.taxi-airport').fadeIn(300);
 		$('.taxi-go_airport').fadeIn(300);
+		$('[class^="bus-"]').hide();
+		$('.bus-airport').fadeIn(300);
 	});
 	$('.package').click(function(){
 		$('[class^="taxi-"]').hide();
@@ -65,7 +69,15 @@ $('document').ready(function(){
 		$('.taxi-day_wise').fadeIn(300);
 		$('.taxi-per_hours').hide();
 	});
-	
+	/* bus Design  */
+	$('.busticket').click(function(){
+		$('[class^="bus-"]').hide();
+		$('.bus-ticket').fadeIn(300);
+	});
+	$('.busesonrent').click(function(){
+		$('[class^="bus-"]').hide();
+		$('.bus-on-rent').fadeIn(300);
+	});
 });
 function topTabDeactive(){
 	$('.topMenu [class^="tab-"] a').removeClass('active');
@@ -87,7 +99,41 @@ function addRow(){
 	    addrow='<tr>';
         addrow+='<td><label>To :</label></td>';
         addrow+='<td><input type="text" placeholder="Pickup Area" required></td>';
-        addrow+='<td><label class="fa fa-minus-square lm20 fa-lg"></label></td>';
+        addrow+='<td><label class="fa fa-minus-square lm20 fa-lg fg-red"></label></td>';
         addrow+='</tr>';
 		$('.addrow').append(addrow);
+}
+
+
+/*  SideBar Menu */
+
+$('document').ready(function(){
+	$('.sideMenu-taxi').click(function(){
+		topmenuHide();
+	   $('.topmenu-taxi').show();
+   	   $('.taxi-section').show();
+	});
+    $('.sideMenu-bus').click(function(){
+			topmenuHide();
+			$('.topmenu-bus').show();
+		    $('.bus-section').show();
+			$('.bus-pop').fadeIn(300);
+	});
+	$('.sideMenu-truck').click(function(){
+		topmenuHide();
+	   $('.topmenu-truck').show();	   
+	});
+	$('.sideMenu-motorCycle').click(function(){
+		topmenuHide();
+	   $('.topmenu-motorCycle').show();
+	});
+});
+function topmenuHide(){
+	$('.topmenu-taxi').hide();
+	$('.topmenu-bus').hide();
+	$('.topmenu-truck').hide();
+	$('.topmenu-motorCycle').hide();
+	
+	$('.taxi-section').hide();
+	$('.bus-section').hide();
 }
